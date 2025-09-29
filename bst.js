a30058fa3374f0230330a635abbfd147c88ba976
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-09-29 10:10:05 +1000

    Clean code: deleteItems() complete

--- bst.js
+++ bst.js
@@ -57,45 +57,30 @@ class Tree {
     let parentNode = null;
 
     const getCurrentNode = (value) => {
-    let traversal = this.root
-  
+      let traversal = this.root;
 
-    while (traversal.data !== value) {
-      parentNode = traversal;
+      while (traversal.data !== value) {
+        parentNode = traversal;
 
-      if (value < traversal.data) {
-      
-           parentNode = traversal
-        traversal = traversal.left;
-      } else if (value > traversal.data) {
-      
-        parentNode = traversal
-        traversal = traversal.right;
-        // if (currentNode.right === value) {
-        //   parentNode = currentNode;
-        // }
-      } else {
-        return "Node not found in leafNode";
+        if (value < traversal.data) {
+          parentNode = traversal;
+          traversal = traversal.left;
+        } else if (value > traversal.data) {
+          parentNode = traversal;
+          traversal = traversal.right;
+        } else {
+          return "Node not found in leafNode";
+        }
       }
-       
-    }
-    console.log(`getCurrentNode result:`)
-    console.log(traversal.data)
-    currentNode = traversal
-   return currentNode
-   
-    }
+      currentNode = traversal;
+     
+    };
 
-   
     //Delete node if leaf
-    console.log(`Start with leaf`)
 
-   getCurrentNode(value)
-    console.log(currentNode.data)
+    getCurrentNode(value);
+ 
     if (currentNode.left === null && currentNode.right === null) {
-        console.log(`Deleting leaf:${value}`)
-      console.log(currentNode.data);
-      console.log(parentNode);
       if (parentNode.left === currentNode) {
         parentNode.left = null;
       }
@@ -103,157 +88,61 @@ class Tree {
         parentNode.right = null;
       }
       currentNode = null;
-      console.log(`-----------complete--------------`)
-     return  prettyPrintBorders(this.root)
-      return this.root;
+
+      return prettyPrintBorders(this.root);
     }
 
     //delete node with one child
- console.log(`Moving to one child node`)
-
-   
-    console.log(`current`)
-    console.log(currentNode.value)
-
-
     if (!currentNode.left || !currentNode.right) {
-getCurrentNode(value)
+      getCurrentNode(value);
 
-        if(currentNode.left){
-      
-  
-         console.log(`deleting node with only left child: ${value}`)
-         console.log(`Current: ${currentNode.data}`)
-         console.log(`Parent: ${parentNode.data}`)
-         if(parentNode.left === currentNode ){
-         parentNode.left = currentNode.left
-         } else if(parentNode.right === currentNode ){
-         parentNode.right = currentNode.left
-         } 
-      
+      if (currentNode.left) {
+        if (parentNode.left === currentNode) {
+          parentNode.left = currentNode.left;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right = currentNode.left;
+        }
+      } else if (currentNode.right) {
+        if (parentNode.left === currentNode) {
+          parentNode.left = currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right = currentNode.right;
+        }
+      }
 
-    } else if (currentNode.right) {
-         console.log(`deleting node with only right child: ${value}`)
-         console.log(`Current: ${currentNode.data}`)
-         console.log(`Parent: ${parentNode.data}`)
-         if(parentNode.left === currentNode ){
-         parentNode.left = currentNode.right
-         } else if(parentNode.right === currentNode ){
-         parentNode.right = currentNode.right
-         } 
-         
-    
+      return prettyPrintBorders(this.root);
     }
-    console.log(`-----------complete--------------`)
-    return prettyPrintBorders(this.root)
-     return this.root;
-}
 
     //delete node with children
-    console.log(`Moving to nodes with both children`)
+   
     if (currentNode.right && currentNode.left) {
-        console.log(`pre function currentNode`)
-        console.log(currentNode.data)
-        getCurrentNode(value)
-        console.log(`Deleting subtree at: ${value}`)
-        console.log(`Parent: ${parentNode.data}`)
-        let successorParent
+      getCurrentNode(value);
+
+      let successorParent;
       let subRoot = currentNode;
-      let successor
+      let successor;
       currentNode = currentNode.right;
-      console.log(`Right branch: ${currentNode.data}`)
 
-if(!currentNode.left){
-    subRoot.right = currentNode.right
-    subRoot.data = currentNode.data
-    
-} else {
-      while(currentNode.left){
-        successorParent = currentNode
-        currentNode = currentNode.left
-        successor = currentNode
+      if (value === this.root) {
       }
-    
-      console.log(currentNode)
-      console.log(`successor: ${successor.data}`)
-      console.log(`successorParent: ${successorParent.data}`)
-    //   console.log(currentNode.data)
-    //   console.log(successor.data)
-    //   console.log(parentNode.data)
-    //   console.log(subRoot.data)
 
-      subRoot.data = successor.data
-      successorParent.left = null
-      console.log(`----------`)
-      
+      if (!currentNode.left) {
+        subRoot.right = currentNode.right;
+        subRoot.data = currentNode.data;
+      } else {
+        while (currentNode.left) {
+          successorParent = currentNode;
+          currentNode = currentNode.left;
+          successor = currentNode;
+        }
+
+        subRoot.data = successor.data;
+        successorParent.left = successor.right;
+      }
+
+      return prettyPrintBorders(this.root);
     }
-
-     
-    
-
-//      try { while (currentNode.left.left !== null) {
-//         currentNode = currentNode.left;
-//       }
-//       const successorParent = currentNode
-    
-//       console.log(`parent: ${parentNode.data}`)
-//       console.log(`subRoot: ${subRoot.data}`)
-//       console.log(`s-parent: ${successorParent.data}`)
-//       console.log(`successor: ${currentNode.left.data}`)
-      
-//        subRoot.data = currentNode.left.data;
-//     // successor.data = deletionNodeData
-//     successorParent.left = currentNode.left.right
-//     console.log(`leftleft`)
-//        }
-    
-
-    
-
-   
-//     catch {while(currentNode.left){
-//         currentNode = currentNode.left
-//     }
-//     console.log(`current ${currentNode.data}`)
-//     console.log(`current right:`) 
-//     console.log(currentNode.right)
-//         subRoot.data = currentNode.data
-//         subRoot.right = currentNode.right
-//         console.log(`left`)
-//     }
-//     finally {console.log(`current ${currentNode.data}`)
-//     console.log(`current right:`) 
-//     console.log(currentNode.right)
-
-   
-
-// }
-  
-    // subRoot.data = successorData
-    // currentNode.data = deletionNodeData
-    //   if(currentNode.right){
-    //     subRoot.right = currentNode.right
-    //   }
-    //   if(currentNode.left){
-    //     subRoot.left = currentNode.left
-    //   }
-    //  let thisNode = this.root
-    //   while(thisNode != currentNode){
-    //     if(thisNode.left === currentNode){
-    //         thisNode.left = currentNode.left
-    //     } else if(thisNode.right === currentNode){
-    //         thisNode.right = currentNode.right
-    //     }
-        
-    // }
-    //     thisNode = null
-    //  this.deleteItem(deletionNodeData)
-    //   parentNode.right === subRoot ? parentNode.right = successor : parentNode.left = successor;
-    console.log(`-----------complete--------------`)
-    return prettyPrintBorders(this.root)   
-    return this.root;
-    }
-     prettyPrintBorders(this.root)
+    prettyPrintBorders(this.root);
   }
 }
 
@@ -353,7 +242,8 @@ const removeSubRoot = (() => {
     bst.deleteItem(12)
 })()
 
-// bst.deleteItem(46)
+bst.deleteItem(73)
+bst.deleteItem(39)
 
 // bst.deleteItem(89)
 
